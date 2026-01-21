2eee7969a720c2c1d0a4345bcd7558a4cc7b44a3
heeemzaaa <hamzaelkhawlani00@gmail.com>
2026-01-21 21:02:56 +0100

    starting on the register page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { RegisterComponent } from './pages/register/register';
 
 export const routes: Routes = [
     {path: 'login', component: Login},
+    { path: 'register', component: RegisterComponent }, // Add this line
     {path: '', redirectTo: 'login', pathMatch: 'full'}
 ];
---
+++ frontend/src/app/pages/register/register.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule, RouterLink], 
+  templateUrl: './register.html',
+  styleUrls: ['./register.css']
+})
+export class RegisterComponent {
+
+  part = 1;
+
+  formData = {
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    about: '',
+    password: ''
+  };
+
+  errorMessage = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onNext() {
+    if (this.part == 1) {
+      this.part = 2;
+    }
+  }
+
+  onBack() {
+    if (this.part == 2) {
+      this.part = 1;
+    }
+  }
+
+  onRegister() {
+    if (!this.formData.email || !this.formData.password || !this.formData.username || this.formData.firstName || this.formData.lastName) {
+      this.errorMessage = 'Please fill in all required fields';
+      return;
+    }
+
+    // Call the Service
+    this.authService.register(this.formData).subscribe({
+      next: (response) => {
+        console.log('Registration successful');
+        
+        // Save token immediately so they don't have to login again
+        if(response.data.token) {
+            this.authService.saveToken(response.data.token);
+        }
+        
+        // Go to Home Page
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        console.error(err);
+        this.errorMessage = 'Registration failed. Email or Username might be taken.';
+      }
+    });
+  }
+}
