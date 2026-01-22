470ec9feb654201ab2a62345a3d2861b6d88055c
heeemzaaa <hamzaelkhawlani00@gmail.com>
2026-01-22 12:57:52 +0100

    login and register handled successfully !

--- frontend/src/app/pages/register/register.ts
+++ frontend/src/app/pages/register/register.ts
@@ -6,7 +6,7 @@ import { AuthService } from '../../services/auth';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule, RouterLink], 
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrls: ['./register.css']
 })
@@ -29,22 +29,46 @@ export class RegisterComponent {
   constructor(
     private authService: AuthService,
     private router: Router
-  ) {}
+  ) { }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.formData.profileImage = reader.result as string;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  removeImage() {
+    this.formData.profileImage = '';
+  }
 
   onNext() {
     if (this.part == 1) {
+      if (!this.formData.firstName || !this.formData.lastName || !this.formData.email || !this.formData.password) {
+        this.errorMessage = 'Please fill in all fields before proceeding.';
+        return;
+      }
       this.part = 2;
+      this.errorMessage = '';
     }
   }
 
   onBack() {
     if (this.part == 2) {
       this.part = 1;
+      this.errorMessage = '';
     }
   }
 
   onRegister() {
-    if (!this.formData.email || !this.formData.password || !this.formData.username || this.formData.firstName || this.formData.lastName) {
+    if (!this.formData.email || !this.formData.password || !this.formData.username || !this.formData.firstName || !this.formData.lastName) {
       this.errorMessage = 'Please fill in all required fields';
       return;
     }
@@ -53,12 +77,12 @@ export class RegisterComponent {
     this.authService.register(this.formData).subscribe({
       next: (response) => {
         console.log('Registration successful');
-        
+
         // Save token immediately so they don't have to login again
-        if(response.data.token) {
-            this.authService.saveToken(response.data.token);
+        if (response.data.token) {
+          this.authService.saveToken(response.data.token);
         }
-        
+
         // Go to Home Page
         this.router.navigate(['/']);
       },
