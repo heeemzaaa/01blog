2e5d515c005ad2361547525dab6ee1ac38228ecf
heeemzaaa <hamzaelkhawlani00@gmail.com>
2026-01-25 22:38:45 +0100

    handling the add post code

--- frontend/src/app/components/add-post/add-post.ts
+++ frontend/src/app/components/add-post/add-post.ts
@@ -1,19 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MediaPreview } from '../../models/media-preview.model';
+import { PostRequest } from '../../models/post-request.model';
+import { PostService } from '../../services/post';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-post',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, FormsModule],
   templateUrl: './add-post.html',
   styleUrl: './add-post.css',
 })
 export class AddPost {
-
+  postService = inject(PostService);
+  router = inject(Router);
   selectedFiles = signal<File[]>([]);
   mediaPreviews = signal<MediaPreview[]>([]);
+  title = signal('');
+  content = signal('');
 
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
@@ -55,8 +62,43 @@ export class AddPost {
     );
   }
 
+  private buildFormData() {
+    const formData = new FormData();
+
+    const postRequest: PostRequest = {
+      title: this.title(),
+      content: this.content(),
+    };
+
+    const postJson = new Blob(
+      [JSON.stringify(postRequest)],
+      { type: 'application/json' }
+    );
+
+    formData.append('post', postJson);
+
+    this.selectedFiles().forEach(file => {
+      formData.append('medias', file);
+    });
+
+    return formData;
+  }
+
   addPost() {
-    console.log('Post submit');
-    console.log(this.selectedFiles());
+    const formData = this.buildFormData();
+
+    this.postService.createPost(formData).subscribe({
+      next: (res) => {
+        this.title.set('');
+        this.content.set('');
+        this.selectedFiles.set([]);
+        this.mediaPreviews.set([]);
+        console.log(res.data);
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        console.error("Post creation failed: ", err);
+      }
+    })
   }
 }
