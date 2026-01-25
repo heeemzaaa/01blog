e9a39cee8700e241bec83a72fd1b7db4dd24d523
heeemzaaa <hamzaelkhawlani00@gmail.com>
2026-01-25 22:52:54 +0100

    handling the error

--- frontend/src/app/pages/homePage/app.router.ts
+++ frontend/src/app/pages/homePage/app.router.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '../../components/home/home';
 import { AddPost } from '../../components/add-post/add-post';
 import { Notifications } from '../../components/notifications/notifications';
+import { Profile } from '../../components/profile/profile';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'add-post', component: AddPost },
   { path: 'notifications', component: Notifications },
+  { path: 'profile', component: Profile },
   // { path: 'post/:id', component: Notifications },
   // { path: 'notifications', component: Notifications },
   // { path: 'notifications', component: Notifications },
