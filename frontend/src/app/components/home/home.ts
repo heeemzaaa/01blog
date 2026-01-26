f874c4a6b9afc34de529c20a46570b7b57b26ad5
heeemzaaa <hamzaelkhawlani00@gmail.com>
2026-01-26 15:06:35 +0100

    handling the component of post

--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -1,15 +1,25 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth'; 
+import { AuthService } from '../../services/auth';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatIconModule], 
+  imports: [CommonModule, MatIconModule],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
 export class HomeComponent {
   currentUser = inject(AuthService).currentUser;
+  isLiked = false;
+  isChosen = false;
+
+  toggleLike() {
+    this.isLiked = !this.isLiked;
+  }
+
+  toggleChosen() {
+    
+  }
 }
